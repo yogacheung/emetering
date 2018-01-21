a49a9f0000e6c41ffe5cdd6d51475608f0ac8986
yogacheung <yauka0215@gmail.com>
2018-01-21 12:55:27 -0600

    4th Commit

---
+++ dailylastreading.php
@@ -0,0 +1,24 @@
+#!/usr/bin/php
+<?php 
+$servername = "localhost";
+$dbusername = "root";
+$dbpassword = "weshallovercomesomeday";
+$dbname = "eMeter";
+
+// Create connection
+$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+	
+$sql = 'INSERT INTO `daily_last_reading`(`Date`, `Unit`, `Reading`) SELECT date_format(`Datetime`, "%Y-%m-%d"), `Unit`, MAX(`Reading`)  FROM `readings` WHERE `Remarks` = "R" AND date_format(`Datetime`, "%Y-%m-%d") = "'.date("Y-m-d",time() – 86400).'" GROUP BY `Unit`;';
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+	//var_dump($result->num_rows);		
+}
+
+$conn->close();
+?>
--- emetering.php
+++ emetering.php
@@ -24,7 +24,7 @@ for($i=$tlength; $i>0; $i--){
 	//echo $startdate.PHP_EOL;
 	//echo $enddate.PHP_EOL;	
 	
-	$sql = 'SELECT date_format(result.`Datetime`,"%Y-%m-%d") AS StartDate, date_format(r2.`Datetime`,"%Y-%m-%d") AS EndDate, result.`Unit`, MAX(r2.`Reading`) - MAX(result.`Reading`) AS Reading FROM (SELECT `Datetime`, `Unit`, `Reading` FROM `readings` WHERE `Remarks` = "R" AND (`Unit`, date_format(`Datetime`, "%Y-%m-%d")) IN (SELECT `Unit`, MIN(date_format(`Datetime`, "%Y-%m-%d")) FROM `readings` WHERE `Remarks` = "R" AND  date_format(`Datetime`, "%Y-%m-%d") BETWEEN "'.$startdate.'" AND "'.$enddate.'" GROUP BY `Unit`)) result,  `readings` r2 WHERE r2.`Remarks` = "R" AND date_format(r2.`Datetime`, "%Y-%m-%d") = "'.$enddate.'" AND r2.`Unit` = result.`Unit` GROUP BY r2.`Unit`, result.`Unit`;';
+	$sql = 'SELECT r1.`Date` AS StartDate, r2.`Date` AS EndDate, r1.`Unit`, r2.`Reading` - r1.`Reading` AS Reading FROM `daily_last_reading` r1, `daily_last_reading` r2 WHERE r1.`Unit` = r2.`Unit` AND (r1.`Unit`, r1.`Date`) IN (SELECT `Unit`, MIN(`Date`) FROM `daily_last_reading` WHERE `Date` BETWEEN "'.$startdate.'" AND "'.$enddate.'" GROUP BY `Unit`) AND r2.`Date` = "'.$enddate.'" ORDER BY r1.`Unit`;';
 	
 	$result = $conn->query($sql);
 
